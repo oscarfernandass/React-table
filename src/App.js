b115b46a9b459bf20e404f0b71d1b393153b70aa
oscarfernandass <125243046+oscarfernandass@users.noreply.github.com>
2023-11-08 10:28:43 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,60 @@
+import './App.css';
+function App() {
+  return (
+    <div id="whole">
+      <h1>#Table</h1>
+      <div id="table">
+        <table>
+
+          <tr>
+            <td>
+              <p className="icon">😊 </p>
+            </td>
+            <td>
+              <p className="icon">🌟 </p>
+            </td>
+            <td>
+              <p className="icon">🌸 </p>
+            </td>
+            <td>
+              <p className="icon">🍔 </p>
+            </td>
+          </tr>
+
+          <tr>
+            <td>
+              <p className="icon">🌸 </p>
+            </td>
+            <td>
+              <p className="icon">🍔 </p>
+            </td>
+            <td>
+              <p className="icon">🎉 </p>
+            </td>
+            <td>
+              <p className="icon">🐱  </p>
+            </td>
+          </tr>
+
+          <tr>
+            <td>
+              <p className="icon">🌞 </p>
+            </td>
+            <td>
+              <p className="icon"> 📚</p>
+            </td>
+            <td>
+              <p className="icon">🎉 </p>
+            </td>
+            <td>
+              <p className="icon">🌍 </p>
+            </td>
+          </tr>
+
+        </table>
+      </div>
+    </div>
+  );
+}
+
+export default App;
